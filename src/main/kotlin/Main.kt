b5003aa8f7c069bdd206b114d16f6f78e3063107
Adisa-Ambenge <124130813+Adisa-Ambenge@users.noreply.github.com>
2023-03-22 15:44:48 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,53 @@
 fun main() {
+val char=firstChar("Angela","Emmy")
+println(char)
+    
 
-val calculator=Calculator(20,2)
-    val result1 = calculator.addition(2, 5)
-    println(result1)
+val calc=Calculator(20,2)
+calc.addition()
+calc.subtraction()
+calc.division()
+calc.multiplication()
 
-    val result2 = calculator.subtraction(80, 50)
-    println(result2)
-
-    val result3 = calculator.division(20, 10)
-    println(result3)
-
-    val result4 = calculator.multiplication(6, 5)
-    println(result4)
-
-    val numbers= arrayOf(33,28,90)
+val results = number(arrayOf(2,45,60,20,56))
+ 
 }
 
-//fun firstChar(string1: String, string2: String): String {
-//    return string1[0]
-//    if (string1.length > string2.length) string1[0]
-//
-//
-//    else if{
-//    (string2.length > string1.length) string2[0]
-//    }
-//    else {
-//        sring1[0]
-//    }
-//}
+fun firstChar(string1: String, string2: String): Char{
+   if (string1.length > string2.length) {
+       string1[0]
+   }
+   else if(string2.length > string1.length){ 
+       return string2[0]  
+   }
+    else {       
+       string1[0]
+   }
+}
 //QUESTION 5
-open class Calculator(var num1: Int,var num2: Int){
-        fun addition(num1: Int, num2:Int): Int {
-            return num1 + num2
+class Calculator(var num1: Int,var num2: Int){
+        fun addition(){
+           println(num1+num2)
         }
 
-        fun subtraction(num1:Int, num2: Int):Int {
-            return num1 - num2
+        fun subtraction(){
+            println(num1 - num2)
         }
 
-        fun division(num1: Int, num2:Int): Int {
+        fun division(){
 
-            return num1 / num2
+         println(num1 / num2)
         }
 
-        fun multiplication(num1: Int, num2: Int): Int {
-            return num1 * num2
+        fun multiplication(){
+          println(num1 * num2)
         }
     }
 
 //QUESTION 2
-fun number(numbers: IntArray): Triple<Int, Int, Double> {
-    val min = numbers.minOrNull() ?: 0
-    val max = numbers.maxOrNull() ?: 0
+fun number(numbers: Array<Int>): Triple<Int, Int, Double> {
+    val min = numbers.min()
+    val max = numbers.max()
     val average = numbers.average()
     return Triple(min, max, average)
 }
